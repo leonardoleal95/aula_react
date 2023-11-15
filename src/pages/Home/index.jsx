94a65a08ca474f77738c83782f3832f04c3e7c46
LEONARDO LEAL <leonardo_leal3@estudante.sesisenai.org.br>
2023-11-14 22:03:28 -0300

    modulo 10 aula 1

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+
+import "./styles.css"
+import imig1  from '../../imagens/img1.jpg';
+
+export function Home() {
+  return (
+    <>
+    <img src={imig1} alt="imagem central" />
+      <h1>O seu próximo destino pode estar aqui...</h1>
+    </>
+
+  )
+}
